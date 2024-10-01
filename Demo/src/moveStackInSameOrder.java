7bc9dad08f3880c9256a5a1c4a5ba705c9a42b7a
Shubham Gupta <153124453+shubham-guptaaa@users.noreply.github.com>
2024-10-02 02:52:12 +0530

    Stack

---
+++ Demo/src/moveStackInSameOrder.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class moveStackInSameOrder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    Stack<Integer> st = new Stack<>();        
+    System.out.println("Enter the number of element in stack you have to enter");
+    int n = sc.nextInt();
+    System.out.println("Enter the element: ");
+    for(int i = 0; i<n; i++){
+        int x = sc.nextInt();
+        st.push(x);
+    }
+    System.out.println(st);
+    Stack<Integer> rev =new Stack<>();
+    System.out.println("Reverse of Stack");
+        while (st.size()>0) {
+            int m = st.peek(); //get the first element of stack
+            rev.push(m); //push the element into other stack;
+            st.pop(); //then pop the element of stack
+        }
+        System.out.println(rev);
+    Stack<Integer> copy = new Stack<>();
+    System.out.println("Copy of the first stack");
+    while(rev.size()>0){
+        copy.push(rev.pop());
+    }
+    System.out.println(copy);
+    }
+    
+}
