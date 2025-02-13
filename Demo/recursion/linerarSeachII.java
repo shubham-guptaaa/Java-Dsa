f76ddbc6d029e4e5637da799b4ca1f50d3a45a3d
Shubham Gupta <153124453+shubham-guptaaa@users.noreply.github.com>
2025-02-13 11:18:44 +0530

    linearSearchII.java

--- Demo/recursion/linerarSeachII.java
+++ Demo/recursion/linerarSeachII.java
@@ -1,3 +1,15 @@
 public class linerarSeachII {
-    
+    public static int searchIdx(int[] arr, int target, int idx){
+        int n = arr.length;
+        if(idx==n) return -1;
+        if(arr[idx]==target) return idx;
+        return searchIdx(arr, target, idx+1);
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,12,54,14,3,8,6,1};
+        int target = 14;
+        int idx = 0;
+        System.out.println(searchIdx(arr, target, idx));
+
+    }
 }
