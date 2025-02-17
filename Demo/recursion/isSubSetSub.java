4f42cd886ab907c59d0bf6f642a273a43e769a2a
Shubham Gupta <153124453+shubham-guptaaa@users.noreply.github.com>
2025-02-17 10:45:13 +0530

    isSubSet.java

--- Demo/recursion/isSubSetSub.java
+++ Demo/recursion/isSubSetSub.java
@@ -1,8 +1,9 @@
 public class isSubSetSub {
     public static boolean helper(int n, int k, int[] a, int idx){
         if(idx==a.length){
-            return false; 
+            return a[idx - 1] == k; // Check if the last element is the target
         }
+        
         return helper(n, k-a[idx], a, idx+1) || helper(n, k, a, idx+1);
     }
     public static boolean isSubsetPresent(int n, int k,int []a) {
@@ -15,13 +16,13 @@ public class isSubSetSub {
     }
     public static void main(String[] args) {
         // Sample Input 1 :
-        // 4 13
-        // 4 3 5 2
+        // 5 14
+        // 4 2 5 6 7
         // Sample Output 1 :
-        // No
-        int[] a ={4,3,5,2};
-        int n = 4;
-        int target = 13;
+        // yes
+        int[] a ={4,2,5,6,7};
+        int n = 5;
+        int target = 14;
         System.out.println(isSubsetPresent(n, target, a));
     }
 }
